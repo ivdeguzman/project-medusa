5ec5b8789e3e00a0dcd4b025b84c7d7ea91171eb
kNaiph <84778729+kNaiph@users.noreply.github.com>
2021-12-02 03:29:33 +0800

    Delete server directory

--- server/routes/EmployeeRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const EmployeeModel = require("../models/EmployeeModel");
-const AttendanceModel = require("../models/AttendanceModel");
-const router = express.Router();
-
-module.exports = router;
